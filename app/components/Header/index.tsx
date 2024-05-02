8a274c49f735bdc2fbbbfdd1a50e1b614434da6e
Dhruvik Neharia <dhruvikn@gmail.com>
2024-05-03 02:59:16 +0530

    feat: update header component with title and subtitle

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,9 +1,23 @@
 import styles from './style.module.css';
+import Link from 'next/link';
 
 export const Header = () => {
   return (
     <>
-      <div className={styles['header-container']}>Header</div>
+      <div className={styles['header-container']}>
+        <h1 className={styles['header--title']}>SQL Dummy IDE</h1>
+
+        <h2 className={styles['header--subtitle']}>
+          Crafted with love on&nbsp;
+          <Link target="_blank" href="https://github.com/dhruvikn/sql-dummy-ide">
+            GitHub
+          </Link>
+          &nbsp;by&nbsp;
+          <Link target="_blank" href="https://linktr.ee/dhruvikn">
+            Dhruvik Neharia
+          </Link>
+        </h2>
+      </div>
     </>
   );
 };
